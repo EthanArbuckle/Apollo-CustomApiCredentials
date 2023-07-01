bb2b2ff7106651c921452d84ba5433cddfeb5b24
Ethan Arbuckle <ea@sourcetheorem.com>
2023-06-30 17:46:57 -0700

    support custom imgur credentials

--- Tweak.m
+++ Tweak.m
@@ -2,6 +2,8 @@
 #import <objc/runtime.h>
 
 static NSString * const kRedditClientID = @"CLIENT_ID_GOES_HERE";
+static NSString * const kImgurClientID = @"IMGUR_CLIENT_ID_GOES_HERE";
+static NSString * const kImgurRapidAPIKey = @"RAPID_API_KEY_GOES_HERE";
 
 
 __attribute__ ((constructor)) static void init(void) {
@@ -15,4 +17,28 @@ __attribute__ ((constructor)) static void init(void) {
 		});
 		method_setImplementation(clientIdMethod, replacementImp);
 	}
+
+	Class _NSURLSessionConfiguration = objc_getClass("NSURLSessionConfiguration");
+	Method setHeadersMethod = class_getInstanceMethod(_NSURLSessionConfiguration, sel_registerName("setHTTPAdditionalHeaders:"));
+	IMP originalSetHeadersImp = method_getImplementation(setHeadersMethod);
+	IMP replacementSetHeadersImp = imp_implementationWithBlock(^void (id _self, NSDictionary *headers) {
+		
+		if (headers && [headers valueForKey:@"Authorization"]) {
+			if ([[headers valueForKey:@"Authorization"] isEqualToString:@"Client-ID 0b596f9aaeef0f4"]) {
+				NSMutableDictionary *newHeaders = [headers mutableCopy];
+				newHeaders[@"Authorization"] = [NSString stringWithFormat:@"Client-ID %@", kImgurClientID];
+				headers = newHeaders;
+			}
+		}
+		
+		if (headers && [headers valueForKey:@"X-RapidAPI-Key"]) {
+			NSMutableDictionary *newHeaders = [headers mutableCopy];
+			newHeaders[@"X-RapidAPI-Key"] = kImgurRapidAPIKey;
+			headers = newHeaders;
+		}
+
+		((void (*)(id, SEL, id))originalSetHeadersImp)(_self, NSSelectorFromString(@"setHTTPAdditionalHeaders:"), headers);
+	});
+
+	method_setImplementation(setHeadersMethod, replacementSetHeadersImp);
 }
